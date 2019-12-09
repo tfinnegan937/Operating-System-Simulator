574f2c9e3fcefe2a9f3f0ae0d40ea007c14dc240
tfinnegan937 <FinneganTimothyD@Gmail.com>
2019-12-09 15:18:01 -0800

    Added Process class

---
+++ Process.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tfinnegan on 12/9/19.
+//
+
+#include "Process.h"
---
+++ Process.h
@@ -0,0 +1,17 @@
+//
+// Created by tfinnegan on 12/9/19.
+//
+
+#ifndef OPERATING_SYSTEM_SIMULATOR_PROCESS_H
+#define OPERATING_SYSTEM_SIMULATOR_PROCESS_H
+#include <chrono>
+#include <
+
+class Process {
+private:
+
+public:
+};
+
+
+#endif //OPERATING_SYSTEM_SIMULATOR_PROCESS_H
