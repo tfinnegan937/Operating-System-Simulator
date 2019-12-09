2a7f8bab574b0b45f5bac714fd00531da887ae12
tfinnegan937 <FinneganTimothyD@Gmail.com>
2019-12-09 15:33:42 -0800

    Completed Process class. TODO: Begin work on scheduler

--- Process.cpp
+++ Process.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Process.h"
+
+bool Process::empty() const {
+    return instruction_queue.empty();
+}
+
+tuple<char, string, int> Process::getNextInstruction(Config * program_config){
+    tuple<char, string, int> front = instruction_queue.back();
+    time_remaining -= get<2>(front) * program_config -> getCycleTime(get<1>(front));
+    instruction_queue.pop_back();
+    return front;
+}
+
+int Process::getTimeRemaining() const{
+    return time_remaining;
+}
+
+Process::Process(queue<tuple<char, string, int> instructions, Config * program_config){
+    while(!instructions.empty()){
+        front = instructions.front();
+        time_remaining += get<2>(front) * program_config->getCycleTime(get<1>(front));
+        instruction_queue.push_back(front);
+        instructions.pop();
+    }
+}
--- Process.h
+++ Process.h
@@ -5,12 +5,20 @@
 #ifndef OPERATING_SYSTEM_SIMULATOR_PROCESS_H
 #define OPERATING_SYSTEM_SIMULATOR_PROCESS_H
 #include <chrono>
-#include <
-
+#include <queue>
+#include <tuple>
+#include <string>
+#include "Config.h"
 class Process {
 private:
-
+    vector<tuple<char, string, int> instruction_queue;
+    int time_remaining;
 public:
+    Process(queue<tuple<char, string, int>> instructions, Config * program_config);
+    int getTimeRemaining() const;
+    tuple<char, string, int> getNextInstruction(Config * program_config);
+    bool empty();
+
 };
 
 
